d31d83dedcb5628ca2a555de1870ed878f7457fc
jerexyz <skymine96@gmail.com>
2019-01-23 14:26:07 +0800

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-a91bf556df9ef566c575.js"
+    "url": "webpack-runtime-ccd42ca6e45139166daf.js"
   },
   {
     "url": "app.a2f933b7ab5f58f2ecfb.css"
@@ -39,7 +39,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "70c90c1686c3b6fb63b38bfe268f0d5c"
+    "revision": "c5d979b4d8f859886bf085bf89bbf038"
   },
   {
     "url": "0-50e6881ec6e485a04f9a.js"
